57a423555b2e6d8c92ca4742d6ee8a93a4a47f9b
Kaustubh Dixit <kaustubh.29.dixit@gmail.com>
2024-01-01 22:59:35 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+function App() {
+	
+	return (
+		<>
+			<div className='mx-auto w-96 mt-96'>
+				<input type='radio' name='radio-4' className='radio radio-accent mr-10' defaultChecked='checked'/>
+				<input type='radio' name='radio-4' className='radio radio-accent'/>
+			</div>
+		</>
+	)
+}
+
+export default App
